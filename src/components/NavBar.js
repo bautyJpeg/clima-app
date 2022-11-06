2afe2883f56f9a35b75532f59ef920a1cd71235c
bautyJpeg <104835461+bautyJpeg@users.noreply.github.com>
2022-11-06 02:09:57 -0300

    clima app

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return(
+
+        <nav className="navbar text-light mb-5">
+            <div className="container-fluid">
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
